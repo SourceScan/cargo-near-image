6b714f1c0248b2537ef983f7e3ace6ac40a9412e
Andrey Gruzdev <agruzdev1@gmail.com>
2024-04-06 12:21:21 +0200

    use builder and args names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Accept user id, group id, and username as arguments
-USER_ID=${1:-0}
-GROUP_ID=${2:-0}
-USER_NAME=${3:-root}
+# Initialize default values
+USER_ID=0
+GROUP_ID=0
+COMMAND_TO_RUN=""
 
-if [ "$USER_ID" != "0" ] || [ "$USER_NAME" != "root" ]; then
+# Process named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --uid) USER_ID="$2"; shift ;;
+        --gid) GROUP_ID="$2"; shift ;;
+        --run) COMMAND_TO_RUN="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ "$USER_ID" != "0" ]; then
+    USER_NAME=builder
     # Ensure /etc/sudoers.d exists
     mkdir -p /etc/sudoers.d
 
-    # Create group and user with specified GID/UID and username
-    groupadd -g $GROUP_ID $USER_NAME || true # Avoid error if group exists
-    useradd -m -u $USER_ID -g $GROUP_ID -s /bin/bash $USER_NAME || true # Avoid error if user exists
+    # Create group and user with specified GID/UID
+    groupadd -g $GROUP_ID $USER_NAME
+    useradd -m -u $USER_ID -g $GROUP_ID -s /bin/bash $USER_NAME
 
     # Add user to sudoers
     echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER_NAME
@@ -20,13 +32,20 @@ if [ "$USER_ID" != "0" ] || [ "$USER_NAME" != "root" ]; then
     # Change ownership of the Rust directories to the new user
     chown -R $USER_ID:$GROUP_ID /usr/local/cargo
     chown -R $USER_ID:$GROUP_ID /usr/local/rustup
-fi
 
-# If additional commands are provided, execute them
-if [ $# -gt 3 ]; then
-    # Remove the first three arguments (UID, GID, USERNAME)
-    shift 3
-    exec gosu $USER_NAME "$@"
+    # If a command is provided, run as non-root builder
+    if [ -n "$COMMAND_TO_RUN" ]; then
+        exec gosu $USER_NAME bash -c "$COMMAND_TO_RUN"
+    else
+        echo "No command specified to run."
+        exit 1
+    fi
 else
-    exec gosu $USER_NAME /bin/bash
+    # If a command is provided, run as root
+    if [ -n "$COMMAND_TO_RUN" ]; then
+        exec bash -c "$COMMAND_TO_RUN"
+    else
+        echo "No command specified to run."
+        exit 1
+    fi
 fi
